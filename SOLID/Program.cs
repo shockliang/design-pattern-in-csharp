3c7cf35a92e007d97d6be6bd45f4f06e7f186dc2
shocklaing <shockwater@gmail.com>
2018-10-11 23:09:46 +0800

    Violating the single responsiblility principle example.

---
+++ SOLID/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SOLID
+{
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count;
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+
+        // Violating the Single Responsibility Principle
+        public void Save(string fileName)
+        {
+            File.WriteAllText(fileName, ToString());
+        }
+
+        // Violating the Single Responsibility Principle
+        public static Journal Load(string fileName)
+        {
+
+        }
+        
+        // Violating the Single Responsibility Principle
+        public void Load(Uri uri)
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var j = new Journal();
+            j.AddEntry("I laughed today!");
+            j.AddEntry("I ate a bug!");
+            Console.WriteLine(j);
+        }
+    }
+}
