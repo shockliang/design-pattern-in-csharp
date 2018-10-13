1bd380d793703a612dc5adca91ede7779418c6af
shocklaing <shockwater@gmail.com>
2018-10-13 15:14:17 +0800

    Violating dependency inversion principle example.

--- SOLID/Program.cs
+++ SOLID/Program.cs
@@ -2,121 +2,64 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using static System.Console;
 
 namespace SOLID
 {
-    public class Document
+    public enum Relationship
     {
-
+        Parent,
+        Child,
+        sibling
     }
 
-    public interface IMachine
+    public class Person
     {
-        void Print(Document document);
-        void Scna(Document document);
-        void Fax(Document document);
+        public string Name;
+        // public DateTime DateOfBirth;
     }
 
-    public class MultiFunctionPrinter : IMachine
+    // low-level
+    public class Relationships
     {
-        public void Fax(Document document)
+        private List<(Person, Relationship, Person)> relations
+            = new List<(Person, Relationship, Person)>();
+
+        public void AddParentAndChild(Person parent, Person child)
         {
-            //
+            relations.Add((parent, Relationship.Parent, child));
+            relations.Add((child, Relationship.Child, parent));
         }
 
-        public void Print(Document document)
-        {
-            //
-        }
-
-        public void Scna(Document document)
-        {
-            //
-        }
+        public List<(Person, Relationship, Person)> Relations => relations;
     }
 
-    // Only print functionality.
-    public class OldFashionedPrinter : IMachine
+    public class Research
     {
-        public void Print(Document document)
+        public Research(Relationships relationships)
         {
-            // implementing
+            var relations = relationships.Relations;
+            foreach (var r in relations.Where(
+                x => x.Item1.Name == "John" &&
+                    x.Item2 == Relationship.Parent
+            ))
+            {
+                WriteLine($"John has a child called {r.Item3.Name}");
+            }
         }
 
-        // Must comment not support in old fashioned print.
-        public void Fax(Document document)
-        {
-            throw new NotImplementedException();
-        }
-
-        // Must comment not support in old fashioned print.
-        public void Scna(Document document)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    public interface IPrinter
-    {
-        void Print(Document document);
-    }
-
-    public interface IScanner
-    {
-        void Scan(Document document);
-    }
-
-    public interface IFaxMachine
-    {
-        void Fax(Document document);
-    }
-
-    public class Photocopier : IPrinter, IScanner
-    {
-        public void Print(Document document)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Scan(Document document)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    public interface IMultiFunctionDevice : IScanner, IPrinter, IFaxMachine
-    {
-
-    }
-
-    public class MultiFunctionMachine : IMultiFunctionDevice
-    {
-        private readonly IPrinter printer;
-        private readonly IScanner scanner;
-        private readonly IFaxMachine faxMachine;
-
-        public MultiFunctionMachine(IPrinter printer, IScanner scanner, IFaxMachine faxMachine)
-        {
-            this.printer = printer ?? throw new ArgumentNullException(nameof(printer));
-            this.scanner = scanner ?? throw new ArgumentNullException(nameof(scanner));
-            this.faxMachine = faxMachine ?? throw new ArgumentNullException(nameof(faxMachine));
-        }
-
-        public void Fax(Document document) => faxMachine.Fax(document);
-
-        public void Print(Document document) => printer.Print(document);
-
-        public void Scan(Document document) => scanner.Scan(document);
-    }
-
-
-
-    class Program
-    {
         static void Main(string[] args)
         {
+            var parent = new Person { Name = "John" };
+            var child1 = new Person { Name = "Chris" };
+            var child2 = new Person { Name = "Mary" };
 
+            var relationships = new Relationships();
+            relationships.AddParentAndChild(parent, child1);
+            relationships.AddParentAndChild(parent, child2);
+
+            var research = new Research(relationships);
         }
     }
 }
