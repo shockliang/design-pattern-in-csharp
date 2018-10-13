cd4067d21d0d12a0e354501d60103b589cbb356a
shocklaing <shockwater@gmail.com>
2018-10-13 15:54:00 +0800

    Without builder.

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using static System.Console;
 
 namespace Builder
 {
@@ -6,7 +8,24 @@ namespace Builder
     {
         static void Main(string[] args)
         {
-            
+            var hello = "hello";
+            var sb = new StringBuilder();
+            sb.Append("<p>");
+            sb.Append(hello);
+            sb.Append("</p>");
+
+            WriteLine(sb.ToString());
+
+            var words = new[] { "hello", "world" };
+            sb.Clear();
+            sb.Append("<ul>");
+            foreach (var word in words)
+            {
+                sb.AppendFormat("<li>{0}</li>", word);
+            }
+            sb.Append("</ul>");
+
+            WriteLine(sb.ToString());
         }
     }
 }
