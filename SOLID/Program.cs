cb2b6ed042c3fbf87a8624ae5eae3a05e7818a12
shocklaing <shockwater@gmail.com>
2018-10-13 14:57:18 +0800

    Interface segregation principle.

--- SOLID/Program.cs
+++ SOLID/Program.cs
@@ -57,6 +57,61 @@ namespace SOLID
         }
     }
 
+    public interface IPrinter
+    {
+        void Print(Document document);
+    }
+
+    public interface IScanner
+    {
+        void Scan(Document document);
+    }
+
+    public interface IFaxMachine
+    {
+        void Fax(Document document);
+    }
+
+    public class Photocopier : IPrinter, IScanner
+    {
+        public void Print(Document document)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(Document document)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IMultiFunctionDevice : IScanner, IPrinter, IFaxMachine
+    {
+
+    }
+
+    public class MultiFunctionMachine : IMultiFunctionDevice
+    {
+        private readonly IPrinter printer;
+        private readonly IScanner scanner;
+        private readonly IFaxMachine faxMachine;
+
+        public MultiFunctionMachine(IPrinter printer, IScanner scanner, IFaxMachine faxMachine)
+        {
+            this.printer = printer ?? throw new ArgumentNullException(nameof(printer));
+            this.scanner = scanner ?? throw new ArgumentNullException(nameof(scanner));
+            this.faxMachine = faxMachine ?? throw new ArgumentNullException(nameof(faxMachine));
+        }
+
+        public void Fax(Document document) => faxMachine.Fax(document);
+
+        public void Print(Document document) => printer.Print(document);
+
+        public void Scan(Document document) => scanner.Scan(document);
+    }
+
+
+
     class Program
     {
         static void Main(string[] args)
