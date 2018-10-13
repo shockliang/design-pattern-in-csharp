54f8386a2b3ce5509c9b9a7442ca18cbe27bddc5
shocklaing <shockwater@gmail.com>
2018-10-13 16:17:09 +0800

    With builder.

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,9 +1,78 @@
 ﻿using System;
 using System.Text;
+using System.Collections.Generic;
 using static System.Console;
 
 namespace Builder
 {
+    public class HtmlElement
+    {
+        public string Name, Text;
+        public List<HtmlElement> Elements = new List<HtmlElement>();
+        private const int indentSize = 2;
+
+        public HtmlElement() { }
+
+        public HtmlElement(string name, string text)
+        {
+            Name = name ?? throw new ArgumentNullException(paramName: nameof(name));
+            Text = text ?? throw new ArgumentNullException(paramName: nameof(text));
+        }
+
+        private string ToStringImpl(int indent)
+        {
+            var sb = new StringBuilder();
+            var i = new string(' ', indentSize * indent);
+
+            sb.AppendLine($"{i}<{Name}>");
+            if (!string.IsNullOrWhiteSpace(Text))
+            {
+                sb.Append(new string(' ', indentSize * (indent + 1)));
+                sb.AppendLine(Text);
+            }
+
+            foreach (var e in Elements)
+            {
+                sb.Append(e.ToStringImpl(indent + 1));
+            }
+            sb.AppendLine($"{i}</{Name}>");
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return ToStringImpl(0);
+        }
+    }
+
+    public class HtmlBuilder
+    {
+        private HtmlElement root = new HtmlElement();
+        private readonly string rootName;
+
+        public HtmlBuilder(string rootName)
+        {
+            this.rootName = rootName;
+            root.Name = rootName;
+        }
+
+        public void AddChild(string childName, string childText)
+        {
+            var e = new HtmlElement(childName, childText);
+            root.Elements.Add(e);
+        }
+
+        public override string ToString()
+        {
+            return root.ToString();
+        }
+
+        public void Clear()
+        {
+            root = new HtmlElement() { Name = rootName };
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -26,6 +95,11 @@ namespace Builder
             sb.Append("</ul>");
 
             WriteLine(sb.ToString());
+
+            var builder = new HtmlBuilder("ul");
+            builder.AddChild("li", "hello");
+            builder.AddChild("li", "world");
+            WriteLine(builder.ToString());
         }
     }
 }
