903757077afb9143e7a9fc88ecee0eb8cbfbf630
shocklaing <shockwater@gmail.com>
2018-10-14 14:16:14 +0800

    Facaded builder.

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -7,52 +7,89 @@ namespace Builder
 {
     public class Person
     {
-        public string Name;
-        public string Position;
+        // Address
+        public string StreetAddress, Postcode, City;
 
-        public class Builder : PersonJobBuilder<Builder>
-        {
-
-        }
-
-        public static Builder New => new Builder();
+        // employment
+        public string CompanyName, Position;
+        public int AnnualIncome;
 
         public override string ToString()
         {
-            return $"{nameof(Name)}: {Name}, {nameof(Position)}: {Position}";
+            return $@"
+                {nameof(StreetAddress)}: {StreetAddress}, 
+                {nameof(Postcode)}: {Postcode}, 
+                {nameof(City)}: {City}, 
+                {nameof(CompanyName)}: {CompanyName}, 
+                {nameof(Position)}: {Position}, 
+                {nameof(AnnualIncome)}: {AnnualIncome}";
         }
     }
 
-    public abstract class PersonBuilder
+    public class PersonBuider   // facade
     {
+        // reference! 
         protected Person person = new Person();
 
-        public Person Build()
+        public PersonJobBuilder Works => new PersonJobBuilder(person);
+        public PersonAddressBuilder Lives => new PersonAddressBuilder(person);
+
+        public static implicit operator Person(PersonBuider personBuider)
         {
-            return person;
+            return personBuider.person;
+        }
+
+    }
+
+    public class PersonAddressBuilder : PersonBuider
+    {
+        public PersonAddressBuilder(Person person)
+        {
+            this.person = person;
+        }
+
+        public PersonAddressBuilder At(string streetAddress)
+        {
+            person.StreetAddress = streetAddress;
+            return this;
+        }
+
+        public PersonAddressBuilder WithPostcode(string postcode)
+        {
+            person.Postcode = postcode;
+            return this;
+        }
+        
+        public PersonAddressBuilder In(string city)
+        {
+            person.City = city;
+            return this;
         }
     }
 
-    public class PersonInfoBuilder<SELF>
-        : PersonBuilder
-        where SELF : PersonInfoBuilder<SELF>
+    public class PersonJobBuilder : PersonBuider
     {
-
-        public SELF Called(string name)
+        public PersonJobBuilder(Person person)
         {
-            person.Name = name;
-            return (SELF) this;
+            this.person = person;
         }
-    }
 
-    public class PersonJobBuilder<SELF>
-        : PersonInfoBuilder<PersonJobBuilder<SELF>>
-        where SELF : PersonJobBuilder<SELF>
-    {
-        public SELF WorkAsA(string position)
+        public PersonJobBuilder At(string companyName)
+        {
+            person.CompanyName = companyName;
+            return this;
+        }
+
+        public PersonJobBuilder AsA(string position)
         {
             person.Position = position;
-            return (SELF) this;
+            return this;
+        }
+
+        public PersonJobBuilder Earning(int amount)
+        {
+            person.AnnualIncome = amount;
+            return this;
         }
     }
 
@@ -60,11 +97,17 @@ namespace Builder
     {
         static void Main(string[] args)
         {
-            var me = Person.New
-                .Called("Shock")
-                .WorkAsA("Engineer")
-                .Build();
-            WriteLine(me);
+            var pb = new PersonBuider();
+            Person person = pb
+                .Lives
+                    .At("No.9487 Somewhere road")
+                    .In("Big city")
+                    .WithPostcode("5487")
+                .Works
+                    .At("Some company")
+                    .AsA("Engineer")
+                    .Earning(87000);
+            WriteLine(person);
         }
     }
 }
