5555e8d6c246a2a0be1286ee6917ac64462e0eda
shocklaing <shockwater@gmail.com>
2018-10-15 20:33:03 +0800

    Point example without factory.

--- Factories/Program.cs
+++ Factories/Program.cs
@@ -2,6 +2,39 @@
 
 namespace Factories
 {
+    public enum CoordinateSystem
+    {
+        Cartesian,
+        Polar
+    }
+
+    public class Point
+    {
+        private double x, y;
+
+        /// <summary>
+        /// Initializes a point from either cartesian or polar 
+        /// </summary>
+        /// <param name="a">x if cartesian, rho if polar</param>
+        /// <param name="b"></param>
+        /// <param name="system"></param>
+        public Point(double a, double b, CoordinateSystem system = CoordinateSystem.Cartesian)
+        {
+            switch (system)
+            {
+                case CoordinateSystem.Cartesian:
+                    x = a;
+                    y = b;
+                    break;
+                case CoordinateSystem.Polar:
+                    x = a * Math.Cos(b);
+                    y = a * Math.Sin(b);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(system), system, null);
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
