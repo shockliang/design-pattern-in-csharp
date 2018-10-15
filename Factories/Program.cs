d97731bc684882d2929606ca08174d988ca2cec7
shocklaing <shockwater@gmail.com>
2018-10-16 01:03:24 +0800

    Inner factory.

--- Factories/Program.cs
+++ Factories/Program.cs
@@ -3,30 +3,14 @@ using static System.Console;
 
 namespace Factories
 {
-    public enum CoordinateSystem
-    {
-        Cartesian,
-        Polar
-    }
 
-    public class PointFactory
-    {
-        public static Point NewCartesianPoint(double x, double y)
-        {
-            return new Point(x, y);
-        }
 
-        public static Point NewPolarPoint(double rho, double theta)
-        {
-            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
-        }
-    }
 
     public class Point
     {
         private double x, y;
 
-        public Point(double x, double y)
+        private Point(double x, double y)
         {
             this.x = x;
             this.y = y;
@@ -36,12 +20,27 @@ namespace Factories
         {
             return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
         }
+
+        public static Point Origin => new Point(0, 0);
+
+        public class Factory
+        {
+            public static Point NewCartesianPoint(double x, double y)
+            {
+                return new Point(x, y);
+            }
+
+            public static Point NewPolarPoint(double rho, double theta)
+            {
+                return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+            }
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            var point = PointFactory.NewPolarPoint(1.0, Math.PI / 2);
+            var point = Point.Factory.NewPolarPoint(1.0, Math.PI / 2);
             WriteLine(point);
         }
     }
