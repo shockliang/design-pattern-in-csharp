32c010fa6cc3e80aad175281516c128a8bd9fd15
shocklaing <shockwater@gmail.com>
2018-10-16 22:44:52 +0800

    Abstract factory.

--- Factories/Program.cs
+++ Factories/Program.cs
@@ -1,47 +1,86 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 using static System.Console;
 
 namespace Factories
 {
-
-
-
-    public class Point
+    public interface IHotDrink
     {
-        private double x, y;
+        void Consume();
+    }
 
-        private Point(double x, double y)
+    internal class Tea : IHotDrink
+    {
+        public void Consume()
         {
-            this.x = x;
-            this.y = y;
-        }
-
-        public override string ToString()
-        {
-            return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
-        }
-
-        public static Point Origin => new Point(0, 0);
-
-        public class Factory
-        {
-            public static Point NewCartesianPoint(double x, double y)
-            {
-                return new Point(x, y);
-            }
-
-            public static Point NewPolarPoint(double rho, double theta)
-            {
-                return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
-            }
+            WriteLine("Drinking hot tea");
         }
     }
+
+    internal class Coffee : IHotDrink
+    {
+        public void Consume()
+        {
+            WriteLine("I LOVE HOT COFFEE!");
+        }
+    }
+
+    public interface IHotDrinkFactory
+    {
+        IHotDrink Prepare(int amount);
+    }
+
+    internal class TeaFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            WriteLine($"Put in a tea bag, boil water, pour {amount} ml, add lemon, enjoy!");
+            return new Tea();
+        }
+    }
+
+    internal class CoffeeFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            WriteLine($"Grind some beans, boil water, pour {amount} ml, enjoy!");
+            return new Coffee();
+        }
+    }
+
+    public class HotDrinkMachine
+    {
+        public enum AvailableDrink
+        {
+            Coffee, Tea
+        }
+
+        private Dictionary<AvailableDrink, IHotDrinkFactory> factories =
+            new Dictionary<AvailableDrink, IHotDrinkFactory>();
+        public HotDrinkMachine()
+        {
+            foreach (AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+            {
+                var factory = (IHotDrinkFactory)Activator.CreateInstance(
+                    Type.GetType($"Factories.{Enum.GetName(typeof(AvailableDrink), drink)}Factory"));
+                factories.Add(drink, factory);
+            }
+        }
+
+        public IHotDrink MakeDrink(AvailableDrink drink, int amount)
+        {
+            return factories[drink].Prepare(amount);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            var point = Point.Factory.NewPolarPoint(1.0, Math.PI / 2);
-            WriteLine(point);
+            var machine = new HotDrinkMachine();
+            var drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Coffee, 500);
+            drink.Consume();
         }
     }
 }
