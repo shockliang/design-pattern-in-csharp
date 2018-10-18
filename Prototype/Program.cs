754c4fe8fb0c8244234643e1ce543a7957d96443
shocklaing <shockwater@gmail.com>
2018-10-19 00:02:05 +0800

    ICloneable is bad.

--- Prototype/Program.cs
+++ Prototype/Program.cs
@@ -1,12 +1,65 @@
 ﻿using System;
+using static System.Console;
 
 namespace Prototype
 {
+    public class Person : ICloneable
+    {
+        public string[] Names;
+        public Address Address;
+
+        public Person(string[] names, Address address)
+        {
+            Names = names ?? throw new ArgumentNullException(nameof(names));
+            Address = address ?? throw new ArgumentNullException(nameof(address));
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Names)}: {string.Join(" ", Names)}, {nameof(Address)}: {Address}";
+        }
+
+        public object Clone()
+        {
+            // shallow copy
+            return new Person(Names, Address.Clone() as Address);
+        }
+    }
+
+    public class Address : ICloneable
+    {
+        public string StreetName;
+        public int HouseNumber;
+
+        public Address(string streetName, int houseNumber)
+        {
+            StreetName = streetName ?? throw new ArgumentNullException(nameof(streetName));
+            HouseNumber = houseNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
+        }
+
+        public object Clone()
+        {
+            return new Address(StreetName, HouseNumber);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            
+            var john = new Person(new string[] { "John", "Smith" },
+                new Address("London Road", 123));
+
+            var jane = john.Clone() as Person;
+            jane.Address.HouseNumber = 789;
+
+            WriteLine(john);
+            WriteLine(jane);
         }
     }
 }
