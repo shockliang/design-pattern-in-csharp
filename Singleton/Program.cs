c4972b0fd8406a219a0c9117e7be688ad58902d2
shocklaing <shockwater@gmail.com>
2018-10-24 22:19:15 +0800

    Singleton implementation.

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,13 +1,49 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using MoreLinq;
 using static System.Console;
 
 namespace Singleton
 {
+    public interface IDatabase
+    {
+        int GetPopulation(string name);
+    }
+
+    public class SingletonDatabase : IDatabase
+    {
+        private Dictionary<string, int> capitals;
+
+        private SingletonDatabase()
+        {
+            WriteLine("Initializing database");
+            capitals = File.ReadAllLines("capitals.txt")
+                .Batch(2)
+                .ToDictionary(
+                    list => list.ElementAt(0).Trim(),
+                    list => int.Parse(list.ElementAt(1))
+                );
+        }
+
+        public int GetPopulation(string name)
+        {
+            return capitals[name];
+        }
+
+        private static Lazy<SingletonDatabase> instance =
+            new Lazy<SingletonDatabase>(() => new SingletonDatabase());
+
+        public static SingletonDatabase Instance => instance.Value;
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            
+            var db = SingletonDatabase.Instance;
+            var city = "Tokyo";
+            WriteLine($"{city} has population {db.GetPopulation(city)}");
         }
     }
 }
