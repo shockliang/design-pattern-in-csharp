d732d6f7828301115039d5c09aef6b834014b533
shocklaing <shockwater@gmail.com>
2018-10-27 11:18:40 +0800

    Monostate pattern for singleton.

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -7,13 +7,37 @@ using static System.Console;
 
 namespace Singleton
 {
+    public class CEO
+    {
+        private static string name;
+        private static int age;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {name}, {nameof(Age)}: {Age}";
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            var db = SingletonDatabase.Instance;
-            var city = "Tokyo";
-            WriteLine($"{city} has population {db.GetPopulation(city)}");
+            var ceo = new CEO();
+            ceo.Name = "Adam Smith";
+            ceo.Age = 55;
+
+            var ceo2 = new CEO();
+            WriteLine(ceo2);
         }
     }
 }
