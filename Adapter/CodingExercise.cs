42129e6f4437126a12e8906c13af7afd652df738
shocklaing <shockwater@gmail.com>
2018-10-28 14:20:14 +0800

    Coding exercise for adapter.

---
+++ Adapter/CodingExercise.cs
@@ -0,0 +1,35 @@
+namespace Adapter
+{
+    public class Square
+    {
+        public int Side;
+    }
+
+    public interface IRectangle
+    {
+        int Width { get; }
+        int Height { get; }
+    }
+
+    public static class ExtensionMethods
+    {
+        public static int Area(this IRectangle rc)
+        {
+            return rc.Width * rc.Height;
+        }
+    }
+
+    public class SquareToRectangleAdapter : IRectangle
+    {
+        public SquareToRectangleAdapter(Square square)
+        {
+            Width = square.Side;
+            Height = square.Side;
+        }
+
+        public int Width { get; private set; }
+
+        public int Height { get; private set; }
+
+    }
+}
