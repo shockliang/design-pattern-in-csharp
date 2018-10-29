14d5dbbdfa014463297f42c91da1b8960da92926
shocklaing <shockwater@gmail.com>
2018-10-29 23:22:16 +0800

    Using autofac for DI container.

--- Bridge/Program.cs
+++ Bridge/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Autofac;
 using static System.Console;
 
 namespace Bridge
@@ -60,13 +61,24 @@ namespace Bridge
     {
         static void Main(string[] args)
         {
-            // var renderer = new RasterRenderer();
-            var renderer = new VectorRenderer();
-            var circle = new Circle(renderer, 5);
-            
-            circle.Draw();
-            circle.Resize(2);
-            circle.Draw();
+            var cb = new ContainerBuilder();
+            cb.RegisterType<VectorRenderer>()
+                .As<IRenderer>()
+                .SingleInstance();
+
+            cb.Register((c, p) =>
+                new Circle(c.Resolve<IRenderer>(),
+                p.Positional<float>(0)));
+                
+            using(var c = cb.Build())
+            {
+                var circle = c.Resolve<Circle>(
+                    new PositionalParameter(0, 5.0f)
+                );
+                circle.Draw();
+                circle.Resize(2);
+                circle.Draw();
+            }
         }
     }
 }
