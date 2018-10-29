af1e4c2bf064b3816edec6fe0f6f04aa6f402a79
shocklaing <shockwater@gmail.com>
2018-10-29 22:24:57 +0800

    Basis bridge pattern implementation.

--- Bridge/Program.cs
+++ Bridge/Program.cs
@@ -3,11 +3,70 @@ using static System.Console;
 
 namespace Bridge
 {
+    public interface IRenderer
+    {
+        void RenderCircle(float radius);
+    }
+
+    public class VectorRenderer : IRenderer
+    {
+        public void RenderCircle(float radius)
+        {
+            WriteLine($"Drawing a circle of radius {radius}");
+        }
+    }
+
+    public class RasterRenderer : IRenderer
+    {
+        public void RenderCircle(float radius)
+        {
+            WriteLine($"Drawing pixels for circle with radius {radius}");
+        }
+    }
+
+    public abstract class Shape
+    {
+        protected IRenderer renderer;
+        public Shape(IRenderer renderer)
+        {
+            this.renderer = renderer ?? throw new ArgumentNullException(nameof(renderer));
+        }
+
+        public abstract void Draw();
+        public abstract void Resize(float factor);
+    }
+
+    public class Circle : Shape
+    {
+        private float radius;
+        public Circle(IRenderer renderer, float radius)
+            : base(renderer)
+        {
+            this.radius = radius;
+        }
+
+        public override void Draw()
+        {
+            renderer.RenderCircle(radius);
+        }
+
+        public override void Resize(float factor)
+        {
+            radius *= factor;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            // var renderer = new RasterRenderer();
+            var renderer = new VectorRenderer();
+            var circle = new Circle(renderer, 5);
             
+            circle.Draw();
+            circle.Resize(2);
+            circle.Draw();
         }
     }
 }
