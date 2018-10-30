e4272871d11518f93656d2ef9bcabddc6cc541f6
shocklaing <shockwater@gmail.com>
2018-10-30 21:55:30 +0800

    Geometric shapes.

---
+++ Composite/Geometry.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composite
+{
+    public class GraphicObject
+    {
+        public virtual string Name { get; set; } = "Group";
+        public string Color;
+        private Lazy<List<GraphicObject>> children = new Lazy<List<GraphicObject>>();
+        public List<GraphicObject> Children => children.Value;
+
+        private void Print(StringBuilder sb, int depth)
+        {
+            sb.Append(new string('\t', depth))
+                .Append(string.IsNullOrWhiteSpace(Color) ? string.Empty : $"{Color} ")
+                .AppendLine(Name);
+
+            foreach (var child in Children)
+            {
+                child.Print(sb, depth + 1);
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            Print(sb, 0);
+            return sb.ToString();
+        }
+    }
+
+    public class Circle : GraphicObject
+    {
+        public override string Name => "Circle";
+    }
+
+    public class Square : GraphicObject
+    {
+        public override string Name => "Square";
+    }
+}
--- Composite/Program.cs
+++ Composite/Program.cs
@@ -7,7 +7,16 @@ namespace Composite
     {
         static void Main(string[] args)
         {
-            
+            var drawing = new GraphicObject { Name = "My Drawing" };
+            drawing.Children.Add(new Square { Color = "Red" });
+            drawing.Children.Add(new Circle { Color = "Yello" });
+
+            var group = new GraphicObject();
+            group.Children.Add(new Circle { Color = "Blue" });
+            group.Children.Add(new Square { Color = "Blue" });
+            drawing.Children.Add(group);
+
+            WriteLine(drawing);
         }
     }
 }
