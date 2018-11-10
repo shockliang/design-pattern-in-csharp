c35725092a32f08863891bab6f627a6fac718ce2
shocklaing <shockwater@gmail.com>
2018-11-10 19:20:09 +0800

    Repeating user names.

--- Flyweight.Test/UnitTest1.cs
+++ Flyweight.Test/UnitTest1.cs
@@ -1,18 +1,91 @@
-using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using Flyweight;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Tests
+namespace Flyweight.Test
 {
+    [TestClass]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+
+        [TestMethod]
+        public void TestUser()
         {
+            var firstNames = Enumerable.Range(0, 100).Select(i => RandomString());
+            var lastNames = Enumerable.Range(0, 100).Select(i => RandomString());
+
+            var users = new List<User>();
+            foreach (var firstName in firstNames)
+            {
+                foreach (var lastName in lastNames)
+                {
+                    users.Add(new User($"{firstName} {lastName}"));
+                }
+            }
+
+            ForceGC();
+            
+            // 410344 bytes
+            var size = GetMemorySize(users);
+
+            Console.WriteLine($"users sieze: {size}");
         }
 
-        [Test]
-        public void Test1()
+        [TestMethod]
+        public void TestUser2()
         {
-            Assert.Pass();
+            var firstNames = Enumerable.Range(0, 100).Select(i => RandomString());
+            var lastNames = Enumerable.Range(0, 100).Select(i => RandomString());
+
+            var users = new List<User2>();
+            foreach (var firstName in firstNames)
+            {
+                foreach (var lastName in lastNames)
+                {
+                    users.Add(new User2($"{firstName} {lastName}"));
+                }
+            }
+
+            ForceGC();
+            
+            // 370346 bytes
+            var size = GetMemorySize(users);
+
+            Console.WriteLine($"users sieze: {size}");
+        }
+
+        private long GetMemorySize(object obj)
+        {
+            long size = 0;
+
+            using (Stream s = new MemoryStream())
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(s, obj);
+                size = s.Length;
+            }
+
+            return size;
+        }
+
+        private void ForceGC()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+
+        private string RandomString()
+        {
+            var rand = new Random();
+            return new string(
+                Enumerable.Range(0, 10)
+                    .Select(i => (char)('a' + rand.Next(26)))
+                    .ToArray());
         }
     }
 }
--- Flyweight/Program.cs
+++ Flyweight/Program.cs
@@ -1,13 +1,52 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 using static System.Console;
 
 namespace Flyweight
 {
+    [Serializable]
+    public class User
+    {
+        private string fullName;
+        public User(string fullName)
+        {
+            this.fullName = fullName;
+        }
+    }
+
+    [Serializable]
+    public class User2
+    {
+        private static List<string> strings = new List<string>();
+        private int[] names;
+        public User2(string fullName)
+        {
+            int getOrAdd(string s)
+            {
+                int idx = strings.IndexOf(s);
+                if (idx != -1)
+                {
+                    return idx;
+                }
+                else
+                {
+                    strings.Add(s);
+                    return strings.Count - 1;
+                }
+            }
+
+            names = fullName.Split(' ').Select(getOrAdd).ToArray();
+        }
+
+        public string FullName => string.Join(" ", names.Select(i => strings[i]));
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            
+
         }
     }
 }
