db05decf66ad9d2ea890f77473e93e225ef1a530
shocklaing <shockwater@gmail.com>
2018-11-11 18:42:42 +0800

    Tex formatting after optimization.

--- Flyweight/FormattedText.cs
+++ Flyweight/FormattedText.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Text;
 
 namespace Flyweight
@@ -33,5 +34,52 @@ namespace Flyweight
 
             return sb.ToString();
         }
+    }
+
+    public class BetterFormattedText
+    {
+        public class TextRange
+        {
+            public int Start, End;
+            public bool Capitalize, Bold, Italic;
+
+            public bool Covers(int position)
+            {
+                return position >= Start && position <= End;
+            }
+        }
+
+        private string plainText;
+        private List<TextRange> formatting = new List<TextRange>();
+
+        public BetterFormattedText(string plainText)
+        {
+            this.plainText = plainText;
+        }
+
+        public TextRange GetRange(int start, int end)
+        {
+            var range = new TextRange() { Start = start, End = end };
+            formatting.Add(range);
+            return range;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                var c = plainText[i];
+                foreach (var range in formatting)
+                {
+                    if (range.Covers(i) && range.Capitalize)
+                        c = char.ToUpper(c);
+                    sb.Append(c);
+                }
+            }
+
+            return sb.ToString();
+        }
+
     }
 }
--- Flyweight/Program.cs
+++ Flyweight/Program.cs
@@ -49,6 +49,10 @@ namespace Flyweight
             var ft = new FormattedText("This is a brave new world");
             ft.Capitalize(10, 15);
             WriteLine(ft);
+
+            var bft = new BetterFormattedText("This is a brave new world");
+            bft.GetRange(10, 15).Capitalize = true;
+            WriteLine(bft);
         }
     }
 }
