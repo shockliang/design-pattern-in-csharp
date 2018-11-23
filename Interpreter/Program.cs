403ef65ca1e9b9d30d920e67198e5c5989c63754
shocklaing <shockwater@gmail.com>
2018-11-23 20:14:22 +0800

    Handmade interpreter: parsing.

--- Interpreter/Program.cs
+++ Interpreter/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using static System.Console;
 
@@ -12,6 +13,65 @@ namespace Interpreter
             string input = "(13+4)-(12+1)";
             var tokens = Lex(input);
             WriteLine(string.Join("\t", tokens));
+            var parsed = Parse(tokens);
+            WriteLine($"{input} = {parsed.Value}");
+        }
+
+        static IElement Parse(IReadOnlyList<Token> tokens)
+        {
+            var result = new BinaryOperation();
+            bool haveLHS = false;
+
+            for (int i = 0; i < tokens.Count; i++)
+            {
+                var token = tokens[i];
+                switch (token.MyType)
+                {
+                    case Token.Type.Integer:
+                        var integer = new Integer(int.Parse(token.Text));
+                        if (!haveLHS)
+                        {
+                            result.Left = integer;
+                            haveLHS = true;
+                        }
+                        else
+                        {
+                            result.Right = integer;
+                        }
+                        break;
+                    case Token.Type.Plus:
+                        result.MyType = BinaryOperation.Type.Addition;
+                        break;
+                    case Token.Type.Minus:
+                        result.MyType = BinaryOperation.Type.Substraction;
+                        break;
+                    case Token.Type.Lparen:
+                        int j = i;
+                        for (; j < tokens.Count; ++j)
+                        {
+                            if (tokens[j].MyType == Token.Type.Rparen)
+                                break;
+                        }
+                        var subexpression = tokens.Skip(i + 1).Take(j - i - 1).ToList();
+                        var element = Parse(subexpression);
+                        if (!haveLHS)
+                        {
+                            result.Left = element;
+                            haveLHS = true;
+                        }
+                        else
+                        {
+                            result.Right = element;
+                        }
+
+                        i = j;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            return result;
         }
 
         static List<Token> Lex(string input)
--- Interpreter/Token.cs
+++ Interpreter/Token.cs
@@ -2,6 +2,48 @@ using System;
 
 namespace Interpreter
 {
+    public interface IElement
+    {
+        int Value { get; }
+    }
+
+    public class Integer : IElement
+    {
+        public int Value { get; }
+
+        public Integer(int value)
+        {
+            this.Value = value;
+        }
+    }
+
+    public class BinaryOperation : IElement
+    {
+        public enum Type
+        {
+            Addition, Substraction
+        }
+
+        public Type MyType;
+        public IElement Left, Right;
+
+        public int Value
+        {
+            get
+            {
+                switch (MyType)
+                {
+                    case Type.Addition:
+                        return Left.Value + Right.Value;
+                    case Type.Substraction:
+                        return Left.Value - Right.Value;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+    }
+
     public class Token
     {
         public enum Type
