940650c11385a01a95a54bc4a37fa3a2be48c32d
shocklaing <shockwater@gmail.com>
2018-11-25 17:21:17 +0800

    Solution for bridge pattern exercise.

--- Bridge/Program.cs
+++ Bridge/Program.cs
@@ -4,81 +4,81 @@ using static System.Console;
 
 namespace Bridge
 {
-    public interface IRenderer
-    {
-        void RenderCircle(float radius);
-    }
+    // public interface IRenderer
+    // {
+    //     void RenderCircle(float radius);
+    // }
 
-    public class VectorRenderer : IRenderer
-    {
-        public void RenderCircle(float radius)
-        {
-            WriteLine($"Drawing a circle of radius {radius}");
-        }
-    }
+    // public class VectorRenderer : IRenderer
+    // {
+    //     public void RenderCircle(float radius)
+    //     {
+    //         WriteLine($"Drawing a circle of radius {radius}");
+    //     }
+    // }
 
-    public class RasterRenderer : IRenderer
-    {
-        public void RenderCircle(float radius)
-        {
-            WriteLine($"Drawing pixels for circle with radius {radius}");
-        }
-    }
+    // public class RasterRenderer : IRenderer
+    // {
+    //     public void RenderCircle(float radius)
+    //     {
+    //         WriteLine($"Drawing pixels for circle with radius {radius}");
+    //     }
+    // }
 
-    public abstract class Shape
-    {
-        protected IRenderer renderer;
-        public Shape(IRenderer renderer)
-        {
-            this.renderer = renderer ?? throw new ArgumentNullException(nameof(renderer));
-        }
+    // public abstract class Shape
+    // {
+    //     protected IRenderer renderer;
+    //     public Shape(IRenderer renderer)
+    //     {
+    //         this.renderer = renderer ?? throw new ArgumentNullException(nameof(renderer));
+    //     }
 
-        public abstract void Draw();
-        public abstract void Resize(float factor);
-    }
+    //     public abstract void Draw();
+    //     public abstract void Resize(float factor);
+    // }
 
-    public class Circle : Shape
-    {
-        private float radius;
-        public Circle(IRenderer renderer, float radius)
-            : base(renderer)
-        {
-            this.radius = radius;
-        }
+    // public class Circle : Shape
+    // {
+    //     private float radius;
+    //     public Circle(IRenderer renderer, float radius)
+    //         : base(renderer)
+    //     {
+    //         this.radius = radius;
+    //     }
 
-        public override void Draw()
-        {
-            renderer.RenderCircle(radius);
-        }
+    //     public override void Draw()
+    //     {
+    //         renderer.RenderCircle(radius);
+    //     }
 
-        public override void Resize(float factor)
-        {
-            radius *= factor;
-        }
-    }
+    //     public override void Resize(float factor)
+    //     {
+    //         radius *= factor;
+    //     }
+    // }
 
     class Program
     {
         static void Main(string[] args)
         {
-            var cb = new ContainerBuilder();
-            cb.RegisterType<VectorRenderer>()
-                .As<IRenderer>()
-                .SingleInstance();
+            // var cb = new ContainerBuilder();
+            // cb.RegisterType<VectorRenderer>()
+            //     .As<IRenderer>()
+            //     .SingleInstance();
 
-            cb.Register((c, p) =>
-                new Circle(c.Resolve<IRenderer>(),
-                p.Positional<float>(0)));
+            // cb.Register((c, p) =>
+            //     new Circle(c.Resolve<IRenderer>(),
+            //     p.Positional<float>(0)));
                 
-            using(var c = cb.Build())
-            {
-                var circle = c.Resolve<Circle>(
-                    new PositionalParameter(0, 5.0f)
-                );
-                circle.Draw();
-                circle.Resize(2);
-                circle.Draw();
-            }
+            // using(var c = cb.Build())
+            // {
+            //     var circle = c.Resolve<Circle>(
+            //         new PositionalParameter(0, 5.0f)
+            //     );
+            //     circle.Draw();
+            //     circle.Resize(2);
+            //     circle.Draw();
+            // }
         }
     }
 }
