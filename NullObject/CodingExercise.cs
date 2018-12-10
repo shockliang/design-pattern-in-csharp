612bcc1e8b84e17134a04bc897415412513b70ff
shocklaing <shockwater@gmail.com>
2018-12-10 22:21:31 +0800

    Solution for null object pattern exercise.

---
+++ NullObject/CodingExercise.cs
@@ -0,0 +1,50 @@
+using System;
+
+namespace NullObject.Exercises
+{
+    public interface ILog
+    {
+        // maximum # of elements in the log
+        int RecordLimit { get; }
+
+        // number of elements already in the log
+        int RecordCount { get; set; }
+
+        // expected to increment RecordCount
+        void LogInfo(string message);
+    }
+
+    public class Account
+    {
+        private ILog log;
+
+        public Account(ILog log)
+        {
+            this.log = log;
+        }
+
+        public void SomeOperation()
+        {
+            int c = log.RecordCount;
+            log.LogInfo("Performing an operation");
+            if (c + 1 != log.RecordCount)
+                throw new Exception();
+            if (log.RecordCount >= log.RecordLimit)
+                throw new Exception();
+        }
+    }
+
+    public class NullLog : ILog
+    {
+        // todo
+        public int RecordLimit { get; private set; } = 1;
+
+        public int RecordCount { get; set; }
+
+        public void LogInfo(string message)
+        {
+            RecordCount++;
+            RecordLimit += RecordCount;
+        }
+    }
+}
