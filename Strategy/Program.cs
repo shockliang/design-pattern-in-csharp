bd584c3245cfee71c6bea543b2d25afc0b7203c4
shocklaing <shockwater@gmail.com>
2018-12-19 20:23:40 +0800

    Dynamic strategy.

--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -7,7 +7,15 @@ namespace Strategy
     {
         static void Main(string[] args)
         {
-            
+            var tp = new TextProcessor();
+            tp.SetOutputFormat(OutputFormat.Markdown);
+            tp.AppendList(new[] { "foo", "bar", "baz" });
+            WriteLine(tp);
+
+            tp.Clear();
+            tp.SetOutputFormat(OutputFormat.Html);
+            tp.AppendList(new[] { "foo", "bar", "baz" });
+            WriteLine(tp);
         }
     }
 }
---
+++ Strategy/TextProcessor.cs
@@ -0,0 +1,87 @@
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy
+{
+    public enum OutputFormat
+    {
+        Markdown,
+        Html
+    }
+
+    public interface IListStrategy
+    {
+        void Start(StringBuilder sb);
+        void End(StringBuilder sb);
+        void AddListItem(StringBuilder sb, string item);
+    }
+
+    public class HtmlListStrategy : IListStrategy
+    {
+        public void AddListItem(StringBuilder sb, string item)
+        {
+            sb.AppendLine($"  <li>{item}</li>");
+        }
+
+        public void End(StringBuilder sb)
+        {
+            sb.AppendLine("</ul>");
+        }
+
+        public void Start(StringBuilder sb)
+        {
+            sb.AppendLine("<ul>");
+        }
+    }
+
+    public class MarkdownListStrategy : IListStrategy
+    {
+        public void AddListItem(StringBuilder sb, string item)
+        {
+            sb.AppendLine($" * {item}");
+        }
+
+        public void End(StringBuilder sb) { }
+
+        public void Start(StringBuilder sb) { }
+    }
+
+    public class TextProcessor
+    {
+        private StringBuilder sb = new StringBuilder();
+        private IListStrategy listStrategy;
+
+        public void SetOutputFormat(OutputFormat format)
+        {
+            switch (format)
+            {
+                case OutputFormat.Markdown:
+                    listStrategy = new MarkdownListStrategy();
+                    break;
+                case OutputFormat.Html:
+                    listStrategy = new HtmlListStrategy();
+                    break;
+            }
+        }
+
+        public void AppendList(IEnumerable<string> items)
+        {
+            listStrategy.Start(sb);
+            foreach (var item in items)
+            {
+                listStrategy.AddListItem(sb, item);
+            }
+            listStrategy.End(sb);
+        }
+
+        public override string ToString()
+        {
+            return sb.ToString();
+        }
+
+        public StringBuilder Clear()
+        {
+            return sb.Clear();
+        }
+    }
+}
