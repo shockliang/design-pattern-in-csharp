b1bc57902c6fe1c317d5716fa70a895c53febd33
shocklaing <shockwater@gmail.com>
2018-12-27 00:59:36 +0800

    Dynamic vistor via the DLR.

--- Vistor/Expression.cs
+++ Vistor/Expression.cs
@@ -4,15 +4,10 @@ using System.Text;
 
 namespace Vistor
 {
-    public interface IExpressionVisitor
-    {
-        void Visit(DoubleExpression de);
-        void Visit(AdditionExpression ae);
-    }
 
     public abstract class Expression
     {
-        public abstract void Accept(IExpressionVisitor visitor);
+
     }
 
     public class DoubleExpression : Expression
@@ -22,12 +17,6 @@ namespace Vistor
         public DoubleExpression(double value)
         {
             this.Value = value;
-        }
-
-        public override void Accept(IExpressionVisitor visitor)
-        {
-            // double dispatch.
-            visitor.Visit(this);
         }
     }
 
@@ -41,49 +30,22 @@ namespace Vistor
             this.Left = left ?? throw new System.ArgumentNullException(nameof(left));
             this.Right = right ?? throw new System.ArgumentNullException(nameof(right));
         }
-
-        public override void Accept(IExpressionVisitor visitor)
-        {
-            visitor.Visit(this);
-        }
     }
 
-    public class ExpressionPrinter : IExpressionVisitor
+    public class ExpressionPrinter
     {
-        private StringBuilder sb = new StringBuilder();
-
-        public void Visit(DoubleExpression de)
-        {
-            sb.Append(de.Value);
-        }
-
-        public void Visit(AdditionExpression ae)
+        public void Print(AdditionExpression ae, StringBuilder sb)
         {
             sb.Append("(");
-            ae.Left.Accept(this);
+            Print((dynamic)ae.Left, sb);
             sb.Append("+");
-            ae.Right.Accept(this);
+            Print((dynamic)ae.Right, sb);
             sb.Append(")");
         }
 
-        public override string ToString() => sb.ToString();
-    }
-
-    public class ExpressionCalculator : IExpressionVisitor
-    {
-        public double Result;
-        public void Visit(DoubleExpression de)
+        public void Print(DoubleExpression de, StringBuilder sb)
         {
-            Result = de.Value;
-        }
-
-        public void Visit(AdditionExpression ae)
-        {
-            ae.Left.Accept(this);
-            var a = Result;
-            ae.Right.Accept(this);
-            var b = Result;
-            Result = a + b;
+            sb.Append(de.Value);
         }
     }
 }
--- Vistor/Program.cs
+++ Vistor/Program.cs
@@ -8,20 +8,16 @@ namespace Vistor
     {
         static void Main(string[] args)
         {
-            var e = new AdditionExpression(
+            Expression e = new AdditionExpression(
                 new DoubleExpression(1),
                 new AdditionExpression(
                     new DoubleExpression(2),
                     new DoubleExpression(3)
                 ));
-
             var ep = new ExpressionPrinter();
-            ep.Visit(e);
-            WriteLine(ep.ToString());
-
-            var calc = new ExpressionCalculator();
-            calc.Visit(e);
-            WriteLine($"{ep} = {calc.Result}");
+            var sb = new StringBuilder();
+            ep.Print((dynamic)e, sb);
+            WriteLine(sb);
         }
     }
 }
