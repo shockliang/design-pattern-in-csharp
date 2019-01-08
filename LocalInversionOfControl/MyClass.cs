9335566bd801756c5b91d9aad0232d77872a22bc
shocklaing <shockwater@gmail.com>
2019-01-08 23:52:30 +0800

    Implementing local inversion of control by extension methods.

---
+++ LocalInversionOfControl/MyClass.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LocalInversionOfControl
+{
+    public static class ExtensionMethods
+    {
+        public struct BoolMarker<T>
+        {
+            public bool Result;
+            public T Self;
+
+            public enum Operation
+            {
+                None,
+                And,
+                Or
+            };
+
+            internal Operation PendingOp;
+
+            internal BoolMarker(bool result, T self, Operating pendingOp)
+            {
+                Result = result;
+                Self = self;
+                PendingOp = pendingOp;
+            }
+
+            public BoolMarker(bool result, T self)
+                : this(result, self, Operation.None)
+            { }
+
+            public BoolMarker<T> And => new BoolMarker<T>(Result, Self, Operation.And);
+
+            public static implicit operator bool(BoolMarker<T> marker)
+            {
+                return marker.Result;
+            }
+        }
+
+        public static T AddTo<T>(this T self, params ICollection<T>[] collections)
+        {
+            foreach (var collection in collections)
+            {
+                collection.Add(self);
+            }
+            return self;
+        }
+
+        public static bool IsOneOf<T>(this T self, params T[] values)
+        {
+            return values.Contains(self);
+        }
+
+        public static BoolMarker<TSubject> HasNo<TSubject, T>(this TSubject self, Func<TSubject, IEnumerable<T>> props)
+        {
+            return new BoolMarker<TSubject>(!props(self).Any(), self);
+        }
+
+        public static BoolMarker<TSubject> HasSome<TSubject, T>(this TSubject self, Func<TSubject, IEnumerable<T>> props)
+        {
+            return new BoolMarker<TSubject>(props(self).Any(), self);
+        }
+
+        public static BoolMarker<T> HasNo<T, U>(this BoolMarker<T> marker, Func<T, IEnumerable<U>> props)
+        {
+            if (marker.PendingOp == BoolMarker<T>.Operation.And && !marker.Result)
+                return marker;
+            return new BoolMarker<T>(!props(marker.Self).Any(), marker.Self);
+        }
+    }
+
+    public class MyClass
+    {
+        public void AddingNumbers()
+        {
+            var list = new List<int>();
+            var list2 = new List<int>();
+            // list.Add(24);
+            24.AddTo(list, list2);
+        }
+
+        public void ProcessCommand(string opcode)
+        {
+            // if(opcode == "AND" || opcode == "OR" || opcode == "XOR") {}
+            // if (new[] { "AND", "OR", "XOR" }.Contains(opcode)) { }
+            // if ("AND OR XOR".Split(' ').Contains(opcode)) { }
+            if (opcode.IsOneOf("AND", "OR", "XOR")) { }
+        }
+
+        public void Process(Person person)
+        {
+            // if (person.Names.Count == 0) { }
+            // if (!person.Names.Any()) { }
+            // if (person.HasNo(p => p.Names)) { }
+            // if (person.HasSome(p => p.Names)) { }
+            if (person.HasSome(p => p.Names).And.HasNo(p => p.Children)) { }
+        }
+    }
+
+    public class Person
+    {
+        public List<string> Names = new List<string>();
+        public List<Person> Children = new List<Person>();
+    }
+}
--- LocalInversionOfControl/Program.cs
+++ LocalInversionOfControl/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace LocalInversionOfControl
 {
@@ -6,7 +7,7 @@ namespace LocalInversionOfControl
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
