8704ee0236124d019c6646ad1f9efbf57b47e76c
shocklaing <shockwater@gmail.com>
2019-01-09 20:32:53 +0800

    Initial DI container project.

---
+++ DIContainer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+namespace DIContainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
