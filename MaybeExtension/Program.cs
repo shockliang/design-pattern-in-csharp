98df0ff8ae518d129f398bca5c30eedbaf8da169
shocklaing <shockwater@gmail.com>
2019-01-15 22:50:50 +0800

    After apply extension.

--- MaybeExtension/Program.cs
+++ MaybeExtension/Program.cs
@@ -14,23 +14,59 @@ namespace MaybeExtension
             else
                 return evaluator(o);
         }
+
+        public static TInput If<TInput>(this TInput o, Func<TInput, bool> evaluator)
+            where TInput : class
+        {
+            if (o == null) return null;
+            return evaluator(o) ? o : null;
+        }
+
+        public static TInput Do<TInput>(this TInput o, Action<TInput> action)
+            where TInput : class
+        {
+            if (o == null) return null;
+            action(o);
+            return o;
+        }
+
+        public static TResult Return<TInput, TResult>(this TInput o,
+            Func<TInput, TResult> evaluator,
+            TResult failureValue)
+            where TInput : class
+        {
+            if (o == null) return failureValue;
+            return evaluator(o);
+        }
+
+        public static TResult WithValue<TInput, TResult>(this TInput o, Func<TInput, TResult> evaluator)
+            where TInput : struct
+        {
+            return evaluator(o);
+        }
     }
     public class MaybeMonadDeemo
     {
         public void MyMethod(Person p)
         {
             string postcode;
-            if (p != null)
-            {
-                if (HasMedicalRecord(p) && p.Address != null)
-                {
-                    CheckAddress(p.Address);
-                    if (p.Address.PostCode != null)
-                        postcode = p.Address.PostCode.ToString();
-                    else
-                        postcode = "UNKNOW";
-                }
-            }
+            // if (p != null)
+            // {
+            //     if (HasMedicalRecord(p) && p.Address != null)
+            //     {
+            //         CheckAddress(p.Address);
+            //         if (p.Address.PostCode != null)
+            //             postcode = p.Address.PostCode.ToString();
+            //         else
+            //             postcode = "UNKNOW";
+            //     }
+            // }
+            string postcode = p.With(x => x.Address).With(x => x.PostCode);
+            postcode = p
+                .If(HasMedicalRecord)
+                .With(x => x.Address)
+                .Do(CheckAddress)
+                .Return(x => x.PostCode, "UNKNOW");
         }
 
         private void CheckAddress(Address address)
